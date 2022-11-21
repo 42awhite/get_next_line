1a933b3636dc649dba1b897c40c1c39c8b266af1
Ana Blanco Mota <ablanco-@c2r7s2.42madrid.com>
2022-11-21 19:23:42 +0100

    first commit

---
+++ get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ablanco- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/21 17:36:24 by ablanco-          #+#    #+#             */
+/*   Updated: 2022/11/21 17:42:31 by ablanco-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unisdt.h>
+# include <stdio.h>
+# include <limits.h>
+# include <stdarg.h>
+# include <fcntl.h>
+
+# ifndef	BUFFER_SIZE
+# define	BUFFERSIZE 42
+#endif
+
+
+#endif
